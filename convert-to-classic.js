e3eea038fff1fb1b09017b3c3d12067913b68538
Jonathan Fraimorice <jonathanfr@wix.com>
2020-12-17 15:55:35 +0200

    fix undefined

--- convert-to-classic.js
+++ convert-to-classic.js
@@ -2,7 +2,7 @@
 try {
 	window.autopilot.log('start script')
 	window.autopilot.log('end script')
-	window.autopilot.reportResult(`Site title is: ${rendererModel.siteInfo.siteTitleSEO}`)
+	window.autopilot.reportResult(`Site title is: ${window.rendererModel.siteInfo.siteTitleSEO}`)
 } catch (e) {
 	window.autopilot.reportError(e)
 }
