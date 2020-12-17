6d692f2c1f106e4933a56b87a98584cf14acb136
Jonathan Fraimorice <jonathanfr@wix.com>
2020-12-17 10:49:23 +0200

    add site title reporting

--- convert-to-classic.js
+++ convert-to-classic.js
@@ -7,6 +7,7 @@ try {
     window.autopilot.log('end script')
 
     window.autopilot.reportResult(`This was a simple ADI to Classic migration on the ${isPublished ? 'published' : 'last saved'} revision`)
+    window.autopilot.reportResult(`Site title is: ${rendererModel.siteInfo.siteTitleSEO}`)
 } catch (e) {
     window.autopilot.reportError(e)
 }
