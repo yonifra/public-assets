0de961a56958072edf1ff4bdf1806917a7de6226
Jonathan Fraimorice <jonathanfr@wix.com>
2021-01-25 18:24:52 +0200

    fix script

--- reiterate-script.js
+++ reiterate-script.js
@@ -1,24 +1,20 @@
-let { componentId, maxIterations } = window.autopilot.payload
-let iterationNumber
+let { componentId, maxIterationsForLastSaved, maxIterationsForPublished } = window.autopilot.payload
+let { iterationNumber, isPublishedRevision } = window.autopilot
 
 const migrate = async () => {
     const pointer = { id: componentId, type: 'DESKTOP' }
 
     const data = documentServices.components.data.get(pointer).text
-    iterationNumber = parseInt(data[data.length - 6]);
+    iterationNumber = parseInt(data[data.length - 6])
 
-    if (iterationNumber === maxIterations) {
-        documentServices.components.data.update(pointer, {text: '<h3 class="font_5">0</h3>'})
-    } else {
-        documentServices.components.data.update(pointer, {text: `<h3 class="font_5">${iterationNumber + 1}</h3>`})
-    }
+    documentServices.components.data.update(pointer, { text: `<h3 class="font_5">${isPublishedRevision ? 'Published' : 'Last Saved'}: ${iterationNumber}</h3>` })
 }
 
 migrate()
     .then(response => {
-        const shouldReiterate = iterationNumber < maxIterations
-		window.autopilot.reportResult(`Site was successfully migrated to MESH (iteration #${iterationNumber})`, {reiterate: shouldReiterate})
+        const shouldReiterate = isPublishedRevision ? iterationNumber < maxIterationsForPublished : iterationNumber < maxIterationsForLastSaved
+		window.autopilot.reportResult(`Site was successfully migrated! (iteration #${iterationNumber})`, {reiterate: shouldReiterate})
 	})
 	.catch(e=> {
-		window.autopilot.reportError(`Failed to migrate site to MESH ${e.message}`)
+		window.autopilot.reportError(`Failed to run site with iterations, reason: ${e.message}`)
 	})
