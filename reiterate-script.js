8518375c5cac93a0d0d6fe2e6a9f3f7f5b59a6bb
Jonathan Fraimorice <jonathanfr@wix.com>
2021-01-25 17:05:25 +0200

    better

--- reiterate-script.js
+++ reiterate-script.js
@@ -1,16 +1,22 @@
-let {componentId} = window.autopilot.payload
+let { componentId, maxIterations } = window.autopilot.payload
+let iterationNumber
 
 const migrate = async () => {
     const pointer = { id: componentId, type: 'DESKTOP' }
 
     const data = documentServices.components.data.get(pointer).text
-    const iterationNumber = parseInt(data[data.length - 6]);
-    documentServices.components.data.update(pointer, {text: `<h3 class="font_5">${iterationNumber + 1}</h3>`})
+    iterationNumber = parseInt(data[data.length - 6]);
+
+    if (iterationNumber === maxIterations) {
+        documentServices.components.data.update(pointer, {text: '<h3 class="font_5">0</h3>'})
+    } else {
+        documentServices.components.data.update(pointer, {text: `<h3 class="font_5">${iterationNumber + 1}</h3>`})
+    }
 }
 
 migrate()
     .then(response => {
-        const shouldReiterate = iterationNumber < 5
+        const shouldReiterate = iterationNumber < maxIterations
 		window.autopilot.reportResult(`Site was successfully migrated to MESH (iteration #${iterationNumber})`, {reiterate: shouldReiterate})
 	})
 	.catch(e=> {
